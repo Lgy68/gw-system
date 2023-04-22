90628c66fadfb819255f133c2d155f52cae51597
ligy <lxy6830@163.com>
2023-04-22 22:32:01 +0800

    解决获取token但登录不进去的问题

---
+++ gw-common/src/main/java/com/xcx/common/annotation/Anonymous.java
@@ -0,0 +1,9 @@
+package com.xcx.common.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Anonymous {
+}
